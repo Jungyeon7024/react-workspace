438e38565d7804e7940e24c5bd795b5667d2a71f
Jungyeon7024 <143372160+Jungyeon7024@users.noreply.github.com>
2023-12-18 17:25:36 +0900

    ObjectExample

--- my-app/src/js/ObjectExample.js
+++ my-app/src/js/ObjectExample.js
@@ -3,7 +3,7 @@ import React, {useState} from "react";
 function ObjectExample(){
 
     const [person, setPerson] = useState ({name: 'kh', age: 20});
-
+    // '' 을 사용한 상태로 사용하면 문자로 인식을해서 +1 을 했을 경우 20111111과 같은 형태로 추가되지만 ''을 지운상태면 숫자로 인식해서 숫자값이 더해진다
     const updateAge = ()=> {
         // ...person : 현재 person 객체를 속성을 펼쳐서 새로운 객체로 만들어주는 역할 객체 속성들은 그대로 복사가 되어서 만들어짐
         // ...person, age 복사된 객체에서 값이 변화하거나 추가해야할 내용이 있으면 , 뒤에 값과 속성을 추가한다. 
