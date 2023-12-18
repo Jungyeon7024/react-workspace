369ae289689087892b493d31b9881760dd0c9cea
Jungyeon7024 <143372160+Jungyeon7024@users.noreply.github.com>
2023-12-18 14:37:18 +0900

    Add

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,16 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Count from './Count';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Count />
   </React.StrictMode>
 );
 
 
 reportWebVitals();
-
+// 주석 표기는 {/* */} react에서는 이렇게 한다. 
 //React.StrictMode : react 내부에서 잠재적인 문제가 있는지 알아내기 위한 도구
 //reportWebVitals  : react 성능을 측정하기 위한 메서드 
